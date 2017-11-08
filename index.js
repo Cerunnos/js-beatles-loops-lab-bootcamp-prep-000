16d6799ff1496670119cda98d9aefc5a0ef349eb
declanm.mulligan@gmail.com Mulligan <declanm.mulligan@gmail.com>
2017-11-08 15:45:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ function theBeatlesPlay(musicians,instruments){
   var emptyArray=[]
   for (var i=0,i<musicians.length,i++){
     emptyArray=[]
+    emptyArray=`${musicians[i]} plays ${instruments[i]}`
   }
 }
